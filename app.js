381fc62400e74d0eaea72146197f7f6cc16a545e
Lawrence Caulker <locaulker@gmail.com>
2023-04-17 05:59:46 -0400

    end of responding to url params

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const fs = require('fs')
-const express = require('express')
+const fs = require('fs');
+const express = require('express');
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 // app.get('/', (req, res) => {
 //   res
@@ -17,38 +17,63 @@ app.use(express.json())
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-)
+);
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
     data: {
-      tours: tours
-    }
-  })
-})
+      tours: tours,
+    },
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+
+  const tour = tours.find((el) => el.id === id);
+
+  // if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tour: tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body)
-  const newId = tours[tours.length - 1].id + 1
-  const newTour = Object.assign({ id: newId }, req.body)
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
 
-  tours.push(newTour)
+  tours.push(newTour);
 
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
-    err => {
+    (err) => {
       res.status(201).json({
         status: 'success',
         data: {
-          tour: newTour
-        }
-      })
-    })
-})
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
 
-const port = 3000
+const port = 3000;
 app.listen(port, () => {
-  console.log(`App running on port ${port}...`)
-})
+  console.log(`App running on port ${port}...`);
+});
