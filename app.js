20b668fe0d1b980d6f5423f44849a8afb9fe9b44
Lawrence Caulker <locaulker@gmail.com>
2023-04-18 05:40:00 -0400

    implemented own middleware

--- app.js
+++ app.js
@@ -3,15 +3,29 @@ const express = require('express');
 
 const app = express();
 
+// Using middle
 app.use(express.json());
+
+// Creating our own middlewares
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 👋');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours: tours,
