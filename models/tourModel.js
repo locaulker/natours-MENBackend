23db292f6f95f6d508a5a86e2344590a6fceb731
Lawrence Caulker <locaulker@gmail.com>
2023-04-27 01:24:31 -0400

    modeled the tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,15 +6,52 @@ const tourSchema = new mongoose.Schema({
     type: 'string',
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a maximum group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: 'number',
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: 'number',
+    default: 0,
   },
   price: {
     type: 'number',
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: 'string',
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
+  description: {
+    type: 'string',
+    trim: true,
+  },
+  imageCover: {
+    type: 'string',
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 // Basic Model
